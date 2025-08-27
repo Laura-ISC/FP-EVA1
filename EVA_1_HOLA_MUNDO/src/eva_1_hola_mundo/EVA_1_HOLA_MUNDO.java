aea6443ac153d22e8c0bdc9ce511dec6f852fbab
Laura-ISC <laujocelyn31@gmail.com>
2025-08-26 21:29:14 -0600

    Add files via upload

---
+++ EVA_1_HOLA_MUNDO/src/eva_1_hola_mundo/EVA_1_HOLA_MUNDO.java
@@ -0,0 +1,10 @@
+package eva_1_hola_mundo;
+
+public class EVA_1_HOLA_MUNDO {
+
+public static void main(String[] args) {
+    System.out.println("HOLA MUNDO!!");
+    System.out.println("FUNDAMENTOS DE PROGRAMACIÓN");
+    }
+    
+}
