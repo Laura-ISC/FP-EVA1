8663345218ab7b04bcf43ca4365a3fcfd59e3e97
Laura-ISC <laujocelyn31@gmail.com>
2025-08-28 10:41:21 -0600

    Add files via upload

---
+++ EVA1_3_DATOS/src/eva1_3_datos/EVA1_3_DATOS.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_datos;
+
+/**
+ *
+ * @author Ana y Lau
+ */
+public class EVA1_3_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Nombre del estudiante: ");
+        System.out.println("Laura Granados");
+        System.out.print("Número de control: ");
+        System.out.println("25550474");
+        System.out.print("Edad: ");
+        System.out.println("18");
+    }
+    
+}
