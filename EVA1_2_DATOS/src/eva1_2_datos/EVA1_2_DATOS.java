3a24bcbc2c33d6c820f1cf04f7a93c00536bb36f
Laura-ISC <laujocelyn31@gmail.com>
2025-08-28 10:40:25 -0600

    Add files via upload

---
+++ EVA1_2_DATOS/src/eva1_2_datos/EVA1_2_DATOS.java
@@ -0,0 +1,26 @@
+package eva1_2_datos;
+
+ class EVA1_2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //; SIGNIFICA FIN DE LINEA
+        // " " CADENA DE TEXTO. DATO
+        System.out.println("LAURA GRANADOS");
+        // NUMEROS ENTEROS
+        System.out.println(1000);
+        // NUMEROS DECIALES
+        System.out.println(500.3);
+        // VALORES LÓGICOS (VERDADERO FALSO)
+        // true false
+        System.out.println(true);
+        // CARACTERES INDIVIDUALES
+        // COSAS DISTINTAS
+        System.out.println('c'); //CARACTER
+        System.out.println("c"); // CADENA DE TEXTO
+    }
+    
+}
