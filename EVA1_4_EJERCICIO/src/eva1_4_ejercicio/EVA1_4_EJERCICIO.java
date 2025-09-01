8ef058422a08b6c8d6ca611a8950979264f5dc8b
Laura-ISC <laujocelyn31@gmail.com>
2025-09-01 12:13:08 -0600

    Add files via upload

---
+++ EVA1_4_EJERCICIO/src/eva1_4_ejercicio/EVA1_4_EJERCICIO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_ejercicio;
+
+/**
+ *
+ * @author Ana y Lau
+ */
+public class EVA1_4_EJERCICIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Empleo: ");
+        System.out.println("Repartidor de comida");
+        System.out.print("Salario: ");
+        System.out.println("5000 semanales");
+        System.out.print("Horas semana: ");
+        System.out.println("6 horas");        
+        System.out.print("¿Requiere inglés? ");
+        System.out.println(false);
+    }
+    
+}
