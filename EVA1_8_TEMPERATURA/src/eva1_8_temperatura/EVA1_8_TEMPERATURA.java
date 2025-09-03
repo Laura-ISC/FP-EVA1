6093d8346aa6dfab11f0a6e5ea244346bc937c71
Laura-ISC <laujocelyn31@gmail.com>
2025-09-03 16:10:41 -0600

    Add files via upload

---
+++ EVA1_8_TEMPERATURA/src/eva1_8_temperatura/EVA1_8_TEMPERATURA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_temperatura;
+
+/**
+ *
+ * @author Ana y Lau
+ */
+public class EVA1_8_TEMPERATURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double Fahrenheit, Celsius, C;
+        Fahrenheit = 50;
+        Celsius = 5*(Fahrenheit - 32) / 9;
+        C = 5*(Fahrenheit - 32)/9 ;
+        System.out.println("50 Fahrenheit a temperatura en Celsius es: ");
+        System.out.println(C);
+        
+    }
+    
+}
